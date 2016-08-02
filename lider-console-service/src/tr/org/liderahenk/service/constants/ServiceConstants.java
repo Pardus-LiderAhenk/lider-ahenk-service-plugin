71e04a38f731003b95f571ebfc2bade48268e5d9
emrekgn <emre.akkaya@agem.com.tr>
2016-08-02 10:23:19 +0300

    Initial commit

---
+++ lider-console-service/src/tr/org/liderahenk/service/constants/ServiceConstants.java
@@ -0,0 +1,9 @@
+package tr.org.liderahenk.service.constants;
+
+public class ServiceConstants {
+	
+	public static final String PLUGIN_NAME = "service";
+	
+	public static final String PLUGIN_VERSION = "1.0.0";
+	
+}
