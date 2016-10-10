2a54a6a79306dc76fc38f522dfecb506a9e5cf8a
cemrealpsoy <cemrealpsoy@gmail.com>
2016-10-10 17:41:32 +0300

    Plugin desc and developer info are added to main.py

--- ahenk-service/service/main.py
+++ ahenk-service/service/main.py
@@ -6,10 +6,10 @@ def info():
     inf['name'] = 'service'
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
-    inf['description'] = ''
+    inf['description'] = 'Agent machine all service settings (service start/stop operations, run a service/services as a start-up service...etc.) are defined with this plugin.'
     inf['task'] = True
     inf['user_oriented'] = False
     inf['machine_oriented'] = False
-    inf['developer'] = ''
+    inf['developer'] = 'cemre.alpsoy@agem.com.tr'
 
     return inf
